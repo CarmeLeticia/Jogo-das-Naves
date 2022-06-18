1b3c16c81366c9c0bde124a791bff462d89b57b8
CarmeLeticia <carme.leticia@gmail.com>
2022-06-18 18:27:45 -0300

    inclusão de movimento das naves

--- js/script.js
+++ js/script.js
@@ -10,8 +10,26 @@ function start() {
 } // Fim da função start
 
 //Principais variáveis do jogo
-
 var jogo = {};
+var velocidade = 5;
+var posicaoY = parseInt(Math.random() * 334);
+var TECLA = {
+  W: 87,
+  S: 83,
+  D: 68,
+};
+
+jogo.pressionou = [];
+
+//Verifica se o usuário pressionou alguma tecla
+
+$(document).keydown(function (e) {
+  jogo.pressionou[e.which] = true;
+});
+
+$(document).keyup(function (e) {
+  jogo.pressionou[e.which] = false;
+});
 
 //Game Loop
 
@@ -19,6 +37,8 @@ jogo.timer = setInterval(loop, 30);
 
 function loop() {
   movefundo();
+  movejogador();
+  moveinimigo1();
 } // Fim da função loop()
 
 //Função que movimenta o fundo do jogo
@@ -27,3 +47,37 @@ function movefundo() {
   esquerda = parseInt($("#fundoGame").css("background-position"));
   $("#fundoGame").css("background-position", esquerda - 1);
 } // fim da função movefundo()
+
+function movejogador() {
+  if (jogo.pressionou[TECLA.W]) {
+    var topo = parseInt($("#jogador").css("top"));
+    $("#jogador").css("top", topo - 10);
+    if (topo <= 0) {
+      $("#jogador").css("top", topo + 10);
+    }
+  }
+
+  if (jogo.pressionou[TECLA.S]) {
+    var topo = parseInt($("#jogador").css("top"));
+    $("#jogador").css("top", topo + 10);
+    if (topo >= 434) {
+      $("#jogador").css("top", topo - 10);
+    }
+  }
+
+  if (jogo.pressionou[TECLA.D]) {
+    //Chama função Disparo
+  }
+} // fim da função movejogador()
+
+function moveinimigo1() {
+  posicaoX = parseInt($("#inimigo1").css("left"));
+  $("#inimigo1").css("left", posicaoX - velocidade);
+  $("#inimigo1").css("top", posicaoY);
+
+  if (posicaoX <= 0) {
+    posicaoY = parseInt(Math.random() * 334);
+    $("#inimigo1").css("left", 694);
+    $("#inimigo1").css("top", posicaoY);
+  }
+} //Fim da função moveinimigo1()
